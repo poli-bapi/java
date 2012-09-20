5f2319a6de6efec81ae86ae752209c23298e8d17
x <x@x-500>
2012-09-20 07:43:37 +0530

    v1

--- build/generated-sources/ap-source-output/com/polidemo/db/Transactions_.java
+++ build/generated-sources/ap-source-output/com/polidemo/db/Transactions_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-09-19T17:11:16")
+@Generated(value="EclipseLink-2.3.0.v20110604-r9504", date="2012-09-20T07:43:15")
 @StaticMetamodel(Transactions.class)
 public class Transactions_ { 
 
